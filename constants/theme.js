851ce072db6fa75c29359085220822266928e2eb
Nitin <rajnitin793@gmail.com>
2026-05-08 13:53:50 +0530

    added theme, saved device hei,wid in /5

---
+++ constants/theme.js
@@ -0,0 +1,30 @@
+export const theme = {
+  colors: {
+    primary: '#1D9BF0',
+    primaryDark: '#1A8CD8',
+    dark: '#0F1419',
+    darkLight: '#16181C',
+    gray: '#EFF3F4',
+
+    text: '#0F1419',
+    textLight: '#536471',
+    textDark: '#AAB8C2',
+
+    rose: '#F4212E',
+    roseLight: '#FF6E79',
+  },
+  fonts: {
+    medium: '500',
+    semibold: '600',
+    bold: '700',
+    extraBold: '800',
+  },
+  radius: {
+    xs: 9999,   
+    sm: 16,
+    md: 12,
+    lg: 8,
+    xl: 4,
+    xxl: 2,
+  },
+}
