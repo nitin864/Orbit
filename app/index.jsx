3e0ac9b2c2d5f7b3a271d2e61f76bd87aee1db90
Nitin <rajnitin793@gmail.com>
2026-05-08 14:10:14 +0530

    linked welcome screen to index page and fixed minor routing issue

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,16 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { use } from 'react'
+import { Button } from '@react-navigation/elements'
+import { useRouter } from 'expo-router'
 
 const index = () => {
+  
+  const router = useRouter()
+
   return (
     <View>
       <Text>index</Text>
+      <Button title='Go to welcome' onPress={()=>router.push('welcome')}/>
     </View>
   ) 
 }
--- components/ScreenWrapper.jsx
+++ components/ScreenWrapper.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+const ScreenWrapper = ({ children, bg }) => {
 
-const ScreenWrapper = ({ children,bg}) => {
-    
-    const {top} = useSAfeAreaInsets();
-    const paddingTop = top>0 ? top+5 : 30
+    const { top } = useSafeAreaInsets();
+    const paddingTop = top > 0 ? top + 5 : 30
 
     return (
-        <View style={{flex: 1, backgroundColor: bg || 'white', paddingTop}}>
+        <View style={{ flex: 1, backgroundColor: bg || 'white', paddingTop }}>
             {children}
         </View>
     );
