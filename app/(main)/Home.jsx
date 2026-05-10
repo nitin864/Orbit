5eae0cf17ee277aae9e1742b9ca2bbc6c27e2ce6
Nitin <rajnitin793@gmail.com>
2026-05-10 18:44:47 +0530

    added logout logic

--- app/(main)/Home.jsx
+++ app/(main)/Home.jsx
@@ -1,11 +1,27 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import ScreenWrapper from '../../components/ScreenWrapper'
+import { Button } from '@react-navigation/elements'
+import { supabase } from '../../lib/supabse'
+import { useAuth } from '../../context/AuthContext'
 
 const Home = () => {
+
+    const {setAuth} = useAuth()
+
+    const handleLogout = async () => {
+         
+        const {error} = await supabase.auth.signOut()
+        if(error){ 
+            Alert.alert('Sign Out', "Error logging out. Please try again.")
+        }
+    }
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <ScreenWrapper>
+        <Text>Home</Text>
+        <Button title = "logout" onPress={handleLogout} />
+    </ScreenWrapper>
   )
 }
 
