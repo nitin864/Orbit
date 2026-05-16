c6a423746b6f5eabf157ce6bc674b59a597ec5ca
Nitin <rajnitin793@gmail.com>
2026-05-16 15:06:19 +0530

    updates in developnment regarding userprofile, noti

--- app/(main)/home.jsx
+++ app/(main)/home.jsx
@@ -1,16 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Pressable, StyleSheet, Text, View } from 'react-native'
 import React, { use } from 'react'
 import ScreenWrapper from '../../components/ScreenWrapper'
 import { Button } from '@react-navigation/elements'
 import { supabase } from '../../lib/supabse'
 import { useAuth } from '../../context/AuthContext'
+import {wp, hp} from '../../helpers/common'
+import {theme} from '../../constants/theme'
+import Icon from '../../assets/icons'
+import { useRoute } from '@react-navigation/native'
+import Avatar from '../../components/Avatar'
 
 
 const Home = () => {
 
     const {setAuth} = useAuth();
      
-
+    const router = useRoute();
+    
     const handleLogout = async () => {
          
         const {error} = await supabase.auth.signOut()
@@ -21,7 +27,28 @@ const Home = () => {
 
   return (
     <ScreenWrapper>
-        <Text>Home</Text>
+        <View style={styles.container}>
+            <View style={styles.header}>
+               <Text style={styles.title}>ORBIT</Text>
+               <View style={styles .icon}>
+                  <Pressable onPress={()=> router.push('notifications')}> 
+                    <Icon name="heart" size={hp(3.2)} color={theme.colors.text} strokeWidth={2} />
+                  </Pressable>
+                  <Pressable onPress={()=> router.push('newPost')}> 
+                    <Icon name="plus" size={ hp(3.2)} color={theme.colors.text} strokeWidth={2} />
+                  </Pressable>
+                  <Pressable onPress={()=> router.push('profile')}> 
+                    <Avatar
+                      uri={user?.image}
+                      size={hp(4.3)}
+                        rounded={theme.radius.sm}
+                        style={borderWidth}
+                    />
+                  </Pressable>
+               </View>
+            </View>
+        </View>
+         
         <Text onPress={handleLogout}>Logout</Text>
     </ScreenWrapper>
   )
@@ -29,4 +56,26 @@ const Home = () => {
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: hp(1)
+    },
+    header: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginBottom: 10,
+        marginHorizontal: wp(4)
+    } ,
+    title: {
+        color: theme.colors.primary,
+        fontSize: hp(3.2),
+        fontWeight: theme.fonts.bold
+    },
+    icon: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 10
+    }
+})
---
+++ components/Avatar.jsx
@@ -0,0 +1,30 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { hp } from '../helpers/common'
+import { theme } from '../constants/theme'
+import {Image} from 'expo-image'
+
+const Avatar = ({
+  uri,
+  size = hp(4.5),
+  rounded = theme.radius.md,
+  style = {}
+}) => {
+  return (
+    <Image  
+      source={uri}
+      transition={100}
+      style={[styles.avatar, {height: size, width: size, borderRadius: rounded}, style]}
+    />
+  )
+}
+
+export default Avatar
+
+const styles = StyleSheet.create({
+  avatar: {
+    borderCurve: 'continuous',
+    backgroundColor: theme.colors.gray[300],
+    borderWidth: 1,
+  } 
+})
