aefbefb79314ac23e26ac24222cf78f1cc18715d
Nitin <rajnitin793@gmail.com>
2026-06-06 12:48:13 +0530

    updates in text_editor

--- components/RichTextEditor.jsx
+++ components/RichTextEditor.jsx
@@ -1,39 +1,66 @@
-import { StyleSheet, View } from 'react-native'
-import { RichToolbar, actions } from 'react-native-pell-rich-editor'
+import { StyleSheet, Text, View } from "react-native";
+import { RichToolbar, actions } from "react-native-pell-rich-editor";
 
-const RichTextEditor = ({
-    editorRef,
-    onChange,
-}) => {
-    return (
-        <View style={{ minHeight: 285 }}>
-            <RichToolbar
-                actions={[
-                    actions.insertImage,
-                    actions.setBold,
-                    actions.setItalic,
-                    actions.insertBulletsList,
-                    actions.insertOrderedList,
-                    actions.insertLink,
-                    actions.keyboard,
-                    actions.setStrikethrough,
-                    actions.setUnderline,
-                    actions.removeFormat,
-                    actions.insertVideo,
-                    actions.checkboxList,
-                    actions.undo,
-                    actions.redo,
-                ]}
-                styles={styles.richbar}
-                flatContainer={styles.richbarContainer}
-                editor={editorRef}
-                disable={false}
-            />
+const RichTextEditor = ({ editorRef }) => {
+  return (
+    <View style={styles.container}>
+      <RichToolbar
+  editor={editorRef}
+  actions={[
+    actions.heading1,
+    actions.heading2,
+    actions.heading3,
 
-        </View>
-    )
-}
+    actions.setBold,
+    actions.setItalic,
+    actions.setUnderline,
+    actions.setStrikethrough,
 
-export default RichTextEditor
+    actions.insertBulletsList,
+    actions.insertOrderedList,
+    actions.checkboxList,
 
-const styles = StyleSheet.create({})
+    actions.insertLink,
+    actions.insertImage,
+    actions.insertVideo,
+
+    actions.undo,
+    actions.redo,
+    actions.removeFormat,
+  ]}
+  iconMap={{
+    [actions.heading1]: ({ tintColor }) => (
+      <Text style={{ color: tintColor, fontWeight: "bold" }}>
+        H1
+      </Text>
+    ),
+    [actions.heading2]: ({ tintColor }) => (
+      <Text style={{ color: tintColor, fontWeight: "bold" }}>
+        H2
+      </Text>
+    ),
+    [actions.heading3]: ({ tintColor }) => (
+      <Text style={{ color: tintColor, fontWeight: "bold" }}>
+        H3
+      </Text>
+    ),
+  }}
+/>
+    </View>
+  );
+};
+
+export default RichTextEditor;
+
+const styles = StyleSheet.create({
+  container: {
+    minHeight: 50,
+  },
+  richbar: {
+    borderRadius: 10,
+  },
+  richbarContainer: {
+    borderRadius: 10,
+    paddingVertical: 8,
+  },
+});
