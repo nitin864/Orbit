ef792246b651e70cda05a7218e50d3a117880455
Nitin Raj <rajnitin793@gmail.com>
2026-09-22 14:35:33 +0530

    updates while project with sdk 57

--- app/index.jsx
+++ app/index.jsx
@@ -1,19 +1,19 @@
-import { View, Text } from 'react-native'
-import React, { use } from 'react'
-import { Button } from '@react-navigation/elements'
-import { useRouter } from 'expo-router'
- 
+import { View } from 'react-native'
+
 import Loading from '../components/Loading'
 
-const index = () => {
-  
-  const router = useRouter() 
- 
+const Index = () => {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Loading/>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Loading />
     </View>
-  ) 
+  )
 }
 
-export default index
+export default Index
