861c4452b1ff4698e24067aabf1adc57ffe174b0
Nitin Raj <rajnitin793@gmail.com>
2026-09-23 11:33:37 +0530

    added a flatlist with initilizing postCard

---
+++ components/PostCard.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const PostCard = () => {
+  return (
+    <View>
+      <Text>PostCard</Text>
+    </View>
+  )
+}
+
+export default PostCard
+
+const styles = StyleSheet.create({})
